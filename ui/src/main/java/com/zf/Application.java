7cd93811bb37e626c75386f870babd1eba2b58c2
ZengFeng <305737657@qq.com>
2016-06-12 16:34:21 +0800

    enable EnableTransactionManagement

--- ui/src/main/java/com/zf/Application.java
+++ ui/src/main/java/com/zf/Application.java
@@ -1,13 +1,17 @@
 package com.zf;
 
+import com.zf.mapper.UserMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.MultipartConfigFactory;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -18,9 +22,13 @@ import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 @EnableCaching
-public class Application {
+@EnableTransactionManagement
+public class Application implements CommandLineRunner {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+    @Autowired
+    UserMapper userMapper;
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
@@ -50,4 +58,9 @@ public class Application {
         return factory.createMultipartConfig();
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        /** 检验数据库配置是否成功 */
+//        System.out.println(this.userMapper.selectUserName("15815800670"));
+    }
 }
