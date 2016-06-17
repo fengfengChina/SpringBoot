5fd4aa12a1e5ee90268e75e96f1dd8e5ef89eff7
ZengFeng <305737657@qq.com>
2016-06-17 10:03:28 +0800

    redis 缓存配置

---
+++ ui/src/main/java/com/zf/config/RedisConfig.java
@@ -0,0 +1,134 @@
+package com.zf.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Method;
+
+/**
+ * RedisConfig
+ *
+ * @author zf
+ * @date 16/6/17
+ */
+@Configuration
+
+@EnableCaching
+public class RedisConfig extends CachingConfigurerSupport {
+    @Autowired
+    private RedisProperty redisProperty;
+
+    private int keyValueTimeOut = 10;
+
+    @Bean
+    public KeyGenerator keyGenerator(){
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(target.getClass().getName());
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };
+    }
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory() {
+        JedisConnectionFactory factory = new JedisConnectionFactory();
+        factory.setHostName(redisProperty.getHost());
+        factory.setPort(redisProperty.getPort());
+        factory.setTimeout(redisProperty.getTimeout()); //设置连接超时时间
+        return factory;
+    }
+    @Bean
+    public CacheManager cacheManager(RedisTemplate redisTemplate) {
+        RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
+        // Number of seconds before expiration. Defaults to unlimited (0)
+        cacheManager.setDefaultExpiration(keyValueTimeOut); //设置key-value超时时间
+        return cacheManager;
+    }
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+        StringRedisTemplate template = new StringRedisTemplate(factory);
+        setSerializer(template); //设置序列化工具，这样ReportBean不需要实现Serializable接口
+        template.afterPropertiesSet();
+        return template;
+    }
+    private void setSerializer(StringRedisTemplate template) {
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+    }
+
+
+}
+@PropertySource(value = "application-dev.properties")
+@Service
+class RedisProperty {
+    /**
+     * 主机地址
+     */
+    @Value("${spring.redis.host}")
+    private String host;
+    /**
+     * 端口号
+     */
+    @Value("${spring.redis.port}")
+    private int port;
+    /**
+     * 超时时间
+     */
+    @Value("${spring.redis.timeout}")
+    private int timeout;
+
+    public RedisProperty() {
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public int getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(int timeout) {
+        this.timeout = timeout;
+    }
+}
